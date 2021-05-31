1fc89d9301e2860370db2d7adaca67261e3d02d0
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-05-31 17:12:19 -0300

    changes on orm config

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,12 @@
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
+  "ssl": true,
+  "extra": {
+    "ssl": {
+      "rejectUnauthorized": false
+    }
+  },
   "entities": [
     process.env.ENTITIES_PATH
   ],
